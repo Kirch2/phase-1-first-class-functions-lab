45e7584885c619a8101cb6f5185d9e424fd7c5ea
Chris <Kirch2@gmail.com>
2021-07-20 23:55:31 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution in this file!
+//['Sally', 'Bob', 'Freddy', 'Claudia']
+
+
+const returnFirstTwoDrivers = drivers => {
+    return [
+        drivers[0],
+        drivers[1]
+    ]
+
+}
+
+const returnLastTwoDrivers = drivers => {
+    return [
+        drivers[2],
+        drivers[3]
+    ]
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+
+const createFareMultiplier = x => {
+    return function (fare) {
+        return x * fare;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = (drivers, func) => {
+    return func(drivers)
+}
+
+
+
+
+
+
